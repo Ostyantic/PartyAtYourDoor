3933e307fbfb7b2fffbdaebb3e851917a7d9c4b3
Anthony Lopez <Antl21292@gmail.com>
2023-12-06 03:36:59 -0500

    added method

--- singerqueue/models.py
+++ singerqueue/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 
@@ -8,3 +9,6 @@ class Singer(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
