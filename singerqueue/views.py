efa1e6c466ce1f05748e0b5e53322aa15116bdb5
Anthony Lopez <Antl21292@gmail.com>
2023-12-06 03:37:15 -0500

    added singercreate view

--- singerqueue/views.py
+++ singerqueue/views.py
@@ -1,8 +1,16 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, CreateView
+from .models import Singer
 
-
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = 'home.html'
+    model = Singer
+    context_object_name = 'singers'
+
+class SingerCreateView(CreateView):
+    template_name = 'create_singer.html'
+    model = Singer
+    fields = ['name', 'song_name']
+
 
 
 class BookMePageView(TemplateView):
