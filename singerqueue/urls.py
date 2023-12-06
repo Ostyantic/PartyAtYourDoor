63f48aecd33d1b082f1b337db5ea5d80b363929c
Anthony Lopez <Antl21292@gmail.com>
2023-12-06 03:37:25 -0500

    updated

--- singerqueue/urls.py
+++ singerqueue/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, BookMePageView
+from .views import HomePageView, BookMePageView, SingerCreateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('create/', SingerCreateView.as_view(), name='create_singer'),
     path('bookme', BookMePageView.as_view(), name='bookme'),
 ]
