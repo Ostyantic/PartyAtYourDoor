df48409d01398bb800c3cd0ac159b644c594eb78
Anthony Lopez <Antl21292@gmail.com>
2023-12-05 20:44:19 -0500

    Home and Bookme urls added

--- singerqueue/urls.py
+++ singerqueue/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views import HomePageView, BookMePageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('bookme', BookMePageView.as_view(), name='bookme'),
+]
