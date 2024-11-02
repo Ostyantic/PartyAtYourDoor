8cc9703af6adfd64625b66a4c0916c56fc5428ee
Anthony Lopez <Antl21292@gmail.com>
2024-11-01 22:15:22 -0400

    registered Contact Info

--- singerqueue/admin.py
+++ singerqueue/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Singer
+from .models import Singer, ContactInfo
 
 admin.site.register(Singer)
+admin.site.register(ContactInfo)
 
 # Register your models here.
